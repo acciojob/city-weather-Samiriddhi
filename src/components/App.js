a5b55b722d51369a2d60dc7174265cf87d60e04e
Samiriddhi <samriddhi.sreyasee123@gmail.com>
2023-11-05 21:23:56 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 
 import React from "react";
 import './../styles/App.css';
+import Search from "./Search";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <Search/>
     </div>
   )
 }
 
 export default App
+
+
